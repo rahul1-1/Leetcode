44b6d2c0e5bb85b57ff76dd04ffa5d3fee706d35
rahul1-1 <68332209+rahul1-1@users.noreply.github.com>
2022-12-09 09:14:28 +0530

    Added solution - LeetHub

---
+++ Black and White - GFG/black-and-white.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long numOfWays(int n, int m);
+
+int main() {
+    // code
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+        cout << numOfWays(n, m) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+//Function to find out the number of ways we can place a black and a 
+//white Knight on this chessboard such that they cannot attack each other.
+long long numOfWays(int n, int m)
+{
+    // write code here
+    long long mod =1e9+7;
+    long long int total = ((n*m)%mod * (n*m-1)%mod)%mod;
+    if(n>=1 &&m>=2)
+    {
+        total-=4*((n-1)%mod * (m-2)%mod)%mod;
+    }
+    if(n>=2 && m>=1)
+    {
+        total-=4*((n-2)%mod *(m-1)%mod)%mod;
+    }
+    return total;
+}
